2391667aeea58eab3048919132e9763d8402b3fc
jin-sj <sj_jin@outlook.com>
2020-03-24 00:45:59 -0700

    typo

--- koreantools/korean_dictionary.py
+++ koreantools/korean_dictionary.py
@@ -1,6 +1,6 @@
 class KoreanDictionary:
     def __int__(self, dictionary_file, frequency_file):
-        self.dictionary_file = dictionay_file
+        self.dictionary_file = dictionary_file
         self.frequency_file = frequency_file
         self.frequency_dictionary = None
 
